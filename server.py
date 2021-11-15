77063095ee3df687fd1ea095d31fe7b78f86154d
jasonxia17 <jasonx3@illinois.edu>
2021-11-15 03:58:01 -0600

    Barebones flask server

---
+++ server.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from hotel_coupon_fsm import fsm_json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def display_homepage():
+    with open("index.html") as f:
+        return f"<script>fsm_json = JSON.parse('{fsm_json}')</script>" + f.read()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
