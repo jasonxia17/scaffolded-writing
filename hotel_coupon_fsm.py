776f300dbf12c0de62a5bb76995e1f40403ce9a4
jasonxia17 <jasonx3@illinois.edu>
2021-11-30 16:26:19 -0600

    Finish inputting FSM

--- hotel_coupon_fsm.py
+++ hotel_coupon_fsm.py
@@ -20,10 +20,6 @@ class State(Enum):
     OUTPUT_QUANTITY_DISTRACTOR_2 = auto()
 
     BEFORE_TRAVELING = auto()
-    AFTER_TRAVELING = auto()
-
-    FOR_I_DISTRACTOR = auto()
-    FOR_I_AND_J_DISTRACTOR = auto()
 
     BEFORE_TRAVEL_ORIGIN = auto()
     AFTER_TRAVEL_ORIGIN = auto()
@@ -40,6 +36,8 @@ class State(Enum):
 fsm: Dict[State, Dict[Tuple[str, ...], State]] = {}
 
 fsm[State.START] = {("Define",): State.BEFORE_DECLARE}
+
+fsm[State.END] = {}
 
 fsm[State.BEFORE_DECLARE] = {
     (
@@ -88,11 +86,67 @@ fsm[State.OUTPUT_QUANTITY_DISTRACTOR_1] = {
 }
 
 fsm[State.OUTPUT_QUANTITY_DISTRACTOR_2] = {
-    (".",): State.OUTPUT_QUANTITY_DISTRACTOR_2,
-    ("that can be used while traveling",): State.AFTER_TRAVELING,
+    ("used",): State.AFTER_OUTPUT_QUANTITY,
 }
 
-# assert fsm.keys() == set(State)
+fsm[State.BEFORE_TRAVELING] = {
+    ("traveling from",): State.BEFORE_TRAVEL_ORIGIN,
+    ("i.", "i and j."): State.END,
+}
+
+fsm[State.BEFORE_TRAVEL_ORIGIN] = {
+    (
+        "Hotel 1",
+        "Hotel n",
+        "Hotel i",
+        "Hotel j",
+        "Hotel k",
+        "the current location",
+    ): State.AFTER_TRAVEL_ORIGIN,
+}
+
+fsm[State.AFTER_TRAVEL_ORIGIN] = {
+    ("to",): State.BEFORE_TRAVEL_DESTINATION,
+}
+
+fsm[State.BEFORE_TRAVEL_DESTINATION] = {
+    (
+        "Hotel 1",
+        "Hotel n",
+        "Hotel i",
+        "Hotel j",
+        "Hotel k",
+        "the current location",
+    ): State.AFTER_TRAVEL_DESTINATION,
+}
+
+fsm[State.AFTER_TRAVEL_DESTINATION] = {
+    (".",): State.END,
+    (",",): State.AFTER_COMMA,
+}
+
+fsm[State.AFTER_COMMA] = {
+    ("under the constraint that",): State.AFTER_UNDER_CONSTRAINT,
+}
+
+fsm[State.AFTER_UNDER_CONSTRAINT] = {
+    ("all remaining",): State.BEFORE_CONSTRAINED_OBJECT,
+    ("at least", "at most", "exactly"): State.BEFORE_CONSTRAINT_AMOUNT,
+}
+
+fsm[State.BEFORE_CONSTRAINT_AMOUNT] = {
+    ("0", "1", "i", "j", "k", "n"): State.BEFORE_CONSTRAINED_OBJECT,
+}
+
+fsm[State.BEFORE_CONSTRAINED_OBJECT] = {
+    ("hotels", "coupons"): State.AFTER_CONSTRAINED_OBJECT,
+}
+
+fsm[State.AFTER_CONSTRAINED_OBJECT] = {
+    ("are used.",): State.END,
+}
+
+assert fsm.keys() == set(State)
 
 # Unroll the tuples, convert enums to int
 processed_fsm: Dict[int, Dict[str, int]] = {}
