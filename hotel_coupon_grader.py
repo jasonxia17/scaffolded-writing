4ed8ba44addc3f8f5bd2349ba5b1b3e03b5e97b7
jasonxia17 <jasonx3@illinois.edu>
2021-12-07 00:00:58 -0600

    Add constraints related to function declaration

--- hotel_coupon_grader.py
+++ hotel_coupon_grader.py
@@ -5,20 +5,18 @@ from typing import Callable, Dict, List, Optional
 
 class Grader:
     def __init__(self, sentence: List[str]):
-        self.parsed_sentence: Dict[State, str] = {}
+        self.parsed_sentence: Dict[State, str] = {state: "" for state in fsm}
 
         curr_state = State.START
         for token in sentence:
-            self.parsed_sentence[curr_state] = token
+            self.parsed_sentence[curr_state] = token.rstrip(".,")
 
             for tokens, next_state in fsm[curr_state].items():
                 if token in tokens:
                     curr_state = next_state
                     break
             else:
-                raise Exception(
-                    f"State {curr_state} does not have a transition for token {token}."
-                )
+                raise Exception(f"State {curr_state} does not have a transition for token {token}.")
 
         print(self.parsed_sentence)
 
@@ -26,6 +24,8 @@ class Grader:
         constraints: List[Callable[[], Optional[str]]] = [
             self.check_output_quantity,
             self.check_extremal_modifier,
+            self.check_function_declaration,
+            self.check_no_undefined_variables,
         ]
 
         for constraint_function in constraints:
@@ -53,9 +53,30 @@ class Grader:
             return f"Are you sure that a subproblem which tries to maximize cost is useful for solving the original problem?"
         return None
 
+    def check_function_declaration(self) -> Optional[str]:
+        function_declaration = self.parsed_sentence[State.FUNCTION_DECLARATION]
 
-print(
-    Grader(
-        ["Define", "MinCost(i)", "to be", "the", "total", "value", "."]
-    ).generate_feedback()
-)
+        if "(" not in function_declaration:
+            return f"Your subproblem definition should declare a function with parameters that can be memoized."
+
+        function_name = function_declaration.split("(")[0]
+
+        if function_name != "MinCost":
+            return (
+                f'Are you sure that "{function_name}" is a good name for your subproblem function?'
+            )
+
+        return None
+
+    def check_no_undefined_variables(self) -> Optional[str]:
+        function_declaration = self.parsed_sentence[State.FUNCTION_DECLARATION]
+
+        is_j_allowed = "(i,j)" in function_declaration
+        if is_j_allowed:
+            return
+
+        if any("j" in token.split() for token in self.parsed_sentence.values()):
+            return "You referred to the variable j, which is not declared as a function parameter nor defined in the original problem."
+
+
+print(Grader(["Define", "MinCost(i)", "to be", "the", "total", "value", "."]).generate_feedback())
