93adb0de7d0a86e6a1e42dca24247e10527bc8be
jasonxia17 <jasonx3@illinois.edu>
2022-01-20 00:54:45 -0600

    Modify CFG to make it compatible with grader

--- cfg.py
+++ cfg.py
@@ -1,26 +1,29 @@
 from nltk.grammar import CFG, is_nonterminal, is_terminal
-from nltk.parse import RecursiveDescentParser
 
 cfg = CFG.fromstring("""
-    START -> "Define" FUNCTION "to be" "the" EXTREMAL_MODIFIER QUANTITY PREPOSITIONAL_PHRASE CONSTRAINT_CLAUSE "."
+    START -> "Define" FUNCTION "to be" "the" EXTREMAL_MODIFIER OUTPUT_QUANTITY PREPOSITIONAL_PHRASE CONSTRAINT_CLAUSE "."
     FUNCTION -> "MinCost(i,j)" | "MinCost(i)" | "DP(i,j)" | "DP(i)" | "the subproblem" | "etc......."
     EXTREMAL_MODIFIER -> "minimum possible" | "maximum possible" |
-    QUANTITY -> "answer" | "value" | "cost" | "number of hotels used" | "number of coupons used"
+    OUTPUT_QUANTITY -> "answer" | "value" | "cost" | "number of hotels used" | "number of coupons used"
     PREPOSITIONAL_PHRASE -> PREPOSITION PREPOSITION_OBJECT
     PREPOSITION -> "of" | "for" | "in" | "while"
-    PREPOSITION_OBJECT -> "i" | "i and j" | "traveling" "from" LOCATION "to" LOCATION
-    LOCATION -> "Hotel 1" | "Hotel n" | "Hotel i" | "Hotel j" | "Hotel k" | "the current location"
+    PREPOSITION_OBJECT -> "i" | "i and j" | "traveling" "from" TRAVEL_ORIGIN "to" TRAVEL_DESTINATION
+    TRAVEL_ORIGIN -> "Hotel 1" | "Hotel i" | "Hotel j" | "Hotel k" | "the current location"
+    TRAVEL_DESTINATION -> "Hotel n" | "Hotel i" | "Hotel j" | "Hotel k" | "the current location"
     CONSTRAINT_CLAUSE -> "," "under the constraint that" CONSTRAINT_STATEMENT |
-    CONSTRAINT_STATEMENT -> "the" QUANTITY "is" COMPARISON_OPERATOR CONSTRAINT_RHS
+    CONSTRAINT_STATEMENT -> "the" CONSTRAINT_QUANTITY "is" COMPARISON_OPERATOR CONSTRAINT_RHS
+    CONSTRAINT_QUANTITY -> "cost" | "number of hotels used" | "number of coupons used"
     COMPARISON_OPERATOR -> "at least" | "at most" | "exactly"
     CONSTRAINT_RHS -> "the number of coupons we have remaining" | "0" | "1" | "i" | "j" | "k" | "n"
 """)
 
-# Check that all nonterminals except START appear on both lhs and rhs at some point
+# check that no nonterminals appear multiple times (that would break the current design of the grader)
+rhs_nonterminals = [symbol for prod in cfg.productions() for symbol in prod.rhs() if is_nonterminal(symbol)]
+assert len(rhs_nonterminals) == len(set(rhs_nonterminals))
+
+# Check that all nonterminals appear on both lhs and rhs (except START, which should only appear on the lhs)
 lhs_nonterminals = {prod.lhs() for prod in cfg.productions()}
-rhs_nonterminals = {symbol for prod in cfg.productions() for symbol in prod.rhs() if is_nonterminal(symbol)}
-assert lhs_nonterminals == rhs_nonterminals | {cfg.start()}, \
-    f"symmetric difference: {lhs_nonterminals ^ rhs_nonterminals}"
+assert lhs_nonterminals - {cfg.start()} == set(rhs_nonterminals)
 
 cfg_as_json = {
     "start": cfg.start().symbol(),
@@ -32,11 +35,3 @@ cfg_as_json = {
         for prod in cfg.productions()
     ]
 }
-
-sentence = ["Define", "MinCost(i,j)", "to be", "the", "minimum possible", "cost", "of", "traveling",
-    "from", "Hotel i", "to", "Hotel n", ",", "under the constraint that", "the", "number of coupons used", "is",
-    "at most", "j", "."]
-
-parser = RecursiveDescentParser(cfg)
-
-print(parser.parse_one(sentence))
