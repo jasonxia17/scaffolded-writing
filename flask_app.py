4ce17ff19c0ea02d5f6b5a3007572d4ee8fe890b
jasonxia17 <nosaj1024@gmail.com>
2022-06-25 19:44:35 -0400

    Move index.html to static directory

--- flask_app.py
+++ flask_app.py
@@ -20,7 +20,7 @@ def handle_submit() -> str:
 
 @app.route("/")
 def display_homepage() -> str:
-    with open("index.html") as f:
+    with open("static/index.html") as f:
         return f"<script>cfg = {json.dumps(cfg_as_json)};</script>" + f.read()
 
 
